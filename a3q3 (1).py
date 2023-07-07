71496d1eb8bc7d6db424b951e257551b74a8bd2e
Harry6542 <ozc189@usak.ca>
2023-07-07 14:31:53 -0600

    Name and information provided

---
+++ a3q3 (1).py
@@ -0,0 +1,21 @@
+# CMPT 145 Course material
+# Copyright (c) 2017-2023 Michael C Horsch
+# All rights reserved.
+#
+# This document contains resources for homework assigned to students of
+# CMPT 145 and shall not be distributed without permission.  Posting this
+# file to a public or private website, or providing this file to a person
+# not registered in CMPT 145, constitutes Academic Misconduct, according
+# to the University of Saskatchewan Policy on Academic Misconduct.
+#
+# Synopsis:
+#    Question nodechain_sumcountreplace starter code
+
+import node as n
+
+
+#Name-Harry Patel
+#NSID-ozc189
+#studentNumber-11358887
+#course-CMPT145
+
