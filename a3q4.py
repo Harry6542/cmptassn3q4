215c976b7c5f22d17eb54a30ff14d348682a4266
Harry6542 <ozc189@usak.ca>
2023-07-07 21:18:47 -0600

    Name and information written

---
+++ a3q4.py
@@ -0,0 +1,18 @@
+# CMPT 145 Course material
+# Copyright (c) 2017-2020 Michael C Horsch
+# All rights reserved.
+#
+# This document contains resources for homework assigned to students of
+# CMPT 145 and shall not be distributed without permission.  Posting this
+# file to a public or private website, or providing this file to a person
+# not registered in CMPT 145, constitutes Academic Misconduct, according
+# to the University of Saskatchewan Policy on Academic Misconduct.
+#
+# Synopsis:
+#     Defines the List ADT
+
+
+#Name-Harry Patel
+#NSID-ozc189
+#studentNumber-11358887
+#course-CMPT145
